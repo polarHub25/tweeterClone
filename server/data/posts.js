3719ed92daa96dab0fd97a86addd0ec70c484dce
polarHub25 <olivetree258@gmail.com>
2023-02-08 21:09:48 +0900

    mongoose 적용
    mongoose 적용 commit

--- server/data/posts.js
+++ server/data/posts.js
@@ -1,68 +1,47 @@
-import { getPosts } from '../database/database.js';
+import Mongoose from 'mongoose';
+import { useVirtualId } from '../database/database.js';
 import * as userRepository from './auth.js';
-import MongoDb from 'mongodb';
-const ObjectId = MongoDb.ObjectId;
+
+const postSchema = new Mongoose.Schema({
+    text: {type: String, required: true},
+    userId: {type: String, required: true},
+    name: {type: String, required: true},
+    username: {type: String, required: true},
+    url: String,
+  }, 
+  {timestamps: true});
+
+useVirtualId(postSchema);
+const Post = Mongoose.model('Post', postSchema);
 
 export async function getAll(){
-    return getPosts()
-    .find()
-    .sort({createdAt: -1})
-    .toArray()
-    .then(mapPosts);
+   return Post.find().sort({createdAt: -1});
 }
 
 export async function getAllByUsername(username){
-    return getPosts()
-    .find({username})
-    .sort({createdAt: -1})
-    .toArray()
-    .then(mapPosts);
+    return Post.find({username}).sort({createdAt: -1});
 }
 
 export async function getById(id){
-    return getPosts()
-    .findOne({_id: new ObjectId(id)})
-    .then(mapOptionalPost);
+    return Post.findById(id);
 }
 
 export async function create(text, userId){
-    const {name, username, url } = await userRepository.findById(userId);
-    const post = {
+    return userRepository.findById(userId)
+    .then((user) =>
+    new Post({
         text,
-        createdAt: new Date(),
         userId,
-        name: name,
-        username: username,
-        url: url,
-    };
-    return getPosts()
-    .insertOne(post)
-    .then((data) => {
-        const newPost = mapOptionalPost({...post, _id: data.insertedId});
-        console.log(newPost);
-        return newPost;
-    })
+        name: user.name,
+        username: user.username,
+    }).save()
+    );
 }
 
 export async function update(id, text){
-   return getPosts()
-   .findOneAndUpdate(
-    {_id: new ObjectId(id)},
-    { $set: {text}},
-    { returnDocument: 'after'} //이걸 명시하지않으면 수정 이전에 해당하는 객체를 받아옴
-   )
-   .then(result => result.value)
-   .then(mapOptionalPost);
+   return Post.findByIdAndUpdate(id, {text}, {returnOriginal: false});
 }
 
 export async function remove(id){
-    return getPosts().deleteOne({_id: new ObjectId(id)});
-}
-
-function mapOptionalPost(post){
-    return post ? {...post, id: post._id.toString()} : post; 
-}
-
-function mapPosts(posts){
-    return posts.map((post) => mapOptionalPost(post));
+    return Post.findByIdAndDelete(id);
 }
--- server/database/database.js
+++ server/database/database.js
@@ -1,18 +1,22 @@
-import MongoDb from 'mongodb';
+import Mongoose from 'mongoose';
 import { config } from '../config.js';
 
-let db;
-
 export async function connectDB(){
-    return MongoDb.MongoClient.connect(config.db.host) //
-    .then((client) => {
-        db = client.db();
-    });
+    return Mongoose.connect(config.db.host);
 }
 
-export function getUsers(){
-    return db.collection('users');
+export function useVirtualId(schema){    
+// _id -> id : mongoDB에서는 _id로 저장되고 정의되서, 읽을때는 id로 바꿔줘야함.
+schema.virtual('id').get(function(){
+    return this._id.toString();
+  });
+  schema.set('toJSON', {virtuals: true});
+  schema.set('toObject', {virtuals: true});
+  
 }
+
+// TODO(Hyemin) : Delete blow 
+let db;
 
 export function getPosts(){
     return db.collection('posts');
