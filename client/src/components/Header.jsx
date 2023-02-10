0c7ae142b8f643c4a1a13265079b46dfc7796061
polarHub25 <olivetree258@gmail.com>
2023-02-10 10:40:08 +0900

    client name modify
    client name modify

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,12 +4,12 @@ const Header = memo(({ username, onLogout, onMyTweets, onAllTweets }) => {
   return (
     <header className='header'>
       <div className='logo'>
-        <img src='./img/logo.png' alt='Dwitter Logo' className='logo-img' />
-        <h1 className='logo-name'>Dwitter</h1>
+        <img src='./img/logo.png' alt='Hwitter Logo' className='logo-img' />
+        <h1 className='logo-name'>Hwitter</h1>
         {username && <span className='logo-user'>@{username}</span>}
       </div>
       {username && (
-        <nav className='menu'>
+        <nav className='menu'>s
           <button onClick={onAllTweets}>All Tweets</button>
           <button onClick={onMyTweets}>My Tweets</button>
           <button className='menu-item' onClick={onLogout}>
